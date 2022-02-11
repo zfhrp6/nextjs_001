2d4d44dc0558fa76b12bc06c0391643244810d85
zfhrp <zfhrp6@users.noreply.github.com>
2022-02-12 02:50:52 +0900

    misc: eslint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   extends: [
     'plugin:react/recommended',
     'airbnb',
+    'next/core-web-vitals',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -21,5 +22,35 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
+    'semi': [2, 'always'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'quotes': ['error', 'single'],
+    'react/function-component-definition': [2, {
+      'namedComponents': 'arrow-function',
+      'unnamedComponents': 'arrow-function',
+      },
+    ],
+    'react/jsx-filename-extension': ['error', {'extensions': ['.jsx', '.tsx'] }],
+    'react/jsx-props-no-spreading': ['error', {
+      'custom': 'ignore',
+      'exceptions': [],
+      },
+    ],
+    'object-curly-newline': ['error', {
+      'ObjectExpression': { 'multiline': true },
+      'ObjectPattern': { 'multiline': true },
+      'ImportDeclaration': { 'multiline': true },
+      'ImportDeclaration': { 'multiline': true },
+      },
+    ],
+    'import/extensions': 'off',
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'no-undef': 'off',
+      },
+    },
+  ],
 };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { RecoilRoot } from "recoil";
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <RecoilRoot>
-      <Component {...pageProps} />
-    </RecoilRoot>
-  )
-}
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <RecoilRoot>
+    <Component {...pageProps} />
+  </RecoilRoot>
+);
 
-export default MyApp
+export default MyApp;
